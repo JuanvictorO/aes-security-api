7f5ce06366e35f78bb13c4d76359e8bee76a9068
J-Luckas <jairoluckas@hotmail.com>
2022-03-22 15:59:57 -0300

    chore: response errors now

--- src/shared/server.ts
+++ src/shared/server.ts
@@ -1,16 +1,14 @@
-import 'dotenv/config';
 import 'reflect-metadata';
+import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
+import 'dotenv/config';
 import './container';
 import {
   initializeTransactionalContext,
   patchTypeORMRepositoryWithBaseRepository,
 } from 'typeorm-transactional-cls-hooked';
-import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 import routes from './routes';
-import * as Sentry from '@sentry/node';
-
-import { errors, isCelebrateError } from 'celebrate';
 import { AppError } from '@shared/errors/AppError';
 
 import '@shared/infra/typeorm';
@@ -28,32 +26,15 @@ app.use(express.json());
 
 app.use(routes);
 
-// Init error handler if not mode development
-if (process.env.APP_MODE !== 'development') {
-  app.use(
-    Sentry.Handlers.errorHandler({
-      shouldHandleError(error) {
-        if (error instanceof AppError || isCelebrateError(error)) {
-          return false;
-        }
-        return true;
-      },
-    }) as express.ErrorRequestHandler,
-  );
-}
-
-app.use(errors());
-
-app.use((err: Error, request: Request, response: Response, _next: NextFunction) => {
-  console.log(`Aqui: ${err}`);
+app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
   if (err instanceof AppError) {
-    return response.status(err.statusCode).json({
-      status: 'error',
-      message: err.message,
-    });
+    return response
+      .status(err.statusCode)
+      .json({
+        status: 'error',
+        message: err.message,
+      });
   }
-
-  console.error(err);
 
   return response.status(500).json({
     status: 'error',

687cf3fb2d2d0272685951c31612f858d3c0bdfb
J-Luckas <jairoluckas@hotmail.com>
2022-03-23 23:07:32 -0300

    chore: initial encrypt to date

--- src/modules/cryptography/entities/DateData.ts
+++ src/modules/cryptography/entities/DateData.ts
@@ -1,13 +1,16 @@
+
 import moment from 'moment';
 import { IData } from '../../interfaces/DataInterface';
 export class DateData implements IData {
   value;
   token;
+  dateMin: string= "1000-01-01";
+
   constructor(value: Date, chaveKey: string) {
     this.value = value;
     this.token = chaveKey;
   }
-
+  
   XOR_hex(a: string, b: string, c: string, d: string) {
     var res = "",
         i = a.length,
@@ -19,35 +22,77 @@ export class DateData implements IData {
     return res;
   }
 
-  getUInt64() {
+  h2d() {
+
     const div: string[] = this.token.match(/.{16}/g) || [];
     const value = this.XOR_hex( div[0], div[1], div[2], div[3]);
-    const fromHexString = (hexString: any) => new Uint8Array(hexString.match(/.{1,2}/g).map((byte: string) => parseInt(byte, 16)));
 
-    const arrayUInt8 = fromHexString(value); // DESCOBRIR COMO CONVERTER O value PARA UInt64;
+    function add(x: any, y: any) {
+        var c = 0, r = [];
+        var x = x.split('').map(Number);
+        var y = y.split('').map(Number);
+        while(x.length || y.length) {
+            var s = (x.pop() || 0) + (y.pop() || 0) + c;
+            r.unshift(s < 10 ? s : s - 10); 
+            c = s < 10 ? 0 : 1;
+        }
+        if(c) r.unshift(c);
+        return r.join('');
+    }
 
-    const valInt = (new BigUint64Array(arrayUInt8.buffer)[0]).toString();
-
-    const mod = parseInt(valInt) % 3652060;
-
-    return mod;
+    var dec = '0';
+    value.split('').forEach(function(chr) {
+        var n = parseInt(chr, 16);
+        for(var t = 8; t; t >>= 1) {
+            dec = add(dec, dec);
+            if(n & t) dec = add(dec, '1');
+        }
+    });
+    return dec;
   }
+
+  // motherfucker( JUMP: number){
+  //   // const mod =  JUMP % 3652060;
+    
+  //   // var VALOR = this.value;
+  //   var VALOR = "9463-07-15"; // this.value;
+  //   var diff = moment(VALOR,"YYYY-MM-DD").diff(moment(this.dateMin,"YYYY-MM-DD")); // Diferença entre a data mínima e a data informada
+
+
+  //   var dias = moment.duration(diff).asDays();
+
+  //   const sumDiasJump = parseInt(dias.toFixed(0) + JUMP);
+
+  //   const emDias = sumDiasJump % 3287176;
+    
+  //   var result = new Date(this.dateMin);
+  //   result.setDate(result.getDate() + emDias);    
+
+  //   // console.log( JUMP );
+  // }
 
   crypt() {
 
-    const mod = this.getUInt64();
-    const date = new Date(this.value);
+    const JUMP = this.h2d();    
+    // Diferença entre a data mínima e a data informada
+    var diff = moment(this.value,"YYYY-MM-DD").diff(moment(this.dateMin,"YYYY-MM-DD"));
+    var dias = moment.duration(diff).asDays();
 
-    date.setDate(date.getDate() + mod);
+    const sumDiasJump = dias.toFixed(0) + JUMP;
 
-    return moment.utc(date).format('YYYY-MM-DD');
+    const emDias = parseInt(sumDiasJump) % 3287176;
+
+    var result = new Date(this.dateMin);
+    result.setDate(result.getDate() + emDias);  
+    
+    return moment.utc(result).format('YYYY-MM-DD');
   }
 
   decrypt( dateCrypted: string ) {
-    const mod = this.getUInt64();
+    const mod = this.h2d();
     const date = new Date(dateCrypted);
 
-    date.setDate(date.getDate() - mod);
+    // date.setDate(date.getDate() - mod);
 
     return moment.utc(date).format('YYYY-MM-DD');
   }

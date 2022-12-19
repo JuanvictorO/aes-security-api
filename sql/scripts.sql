f2d93eaf89386b95e40e8c955a6f1be3396094ae
Juan Victor Oliveira <juan.silva@terceirizadoraizen.com>
2022-12-19 16:27:44 -0300

    added script sql

--- sql/scripts.sql
+++ sql/scripts.sql
@@ -1,7 +1,2 @@
-INSERT INTO `client` (id, auth_token, encrypt_key, seed)
-  VALUES ('', '', '', '');
-  
-INSERT INTO `field_type` (type_name)
-    VALUES ('int'),
-      ('string'),
-      ('date');
+insert into client (id) values ('37c52ac7-e4b9-48f7-ab77-49f98416b0b8'), ('3e60f30a-d3e7-4f26-ae70-9a367c16d609');
+insert into `field_type` (id, type_name) values (1, 'STRING'), (2, 'INT'), (3, 'DATE');

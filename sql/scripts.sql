188b77993954895533a525d997d61cf345f0fd88
Juan Victor Oliveira <juan.silva@terceirizadoraizen.com>
2023-06-21 20:47:42 -0300

    atualizando script com token

--- sql/scripts.sql
+++ sql/scripts.sql
@@ -1,2 +1,3 @@
-insert into client (id) values ('37c52ac7-e4b9-48f7-ab77-49f98416b0b8'), ('3e60f30a-d3e7-4f26-ae70-9a367c16d609');
+insert into client (id, token) values ('37c52ac7-e4b9-48f7-ab77-49f98416b0b8', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2ODczODk3MDIsImV4cCI6MTcxODkyNTcwMiwic3ViIjoiMzdjNTJhYzctZTRiOS00OGY3LWFiNzctNDlmOTg0MTZiMGI4In0.fr7PsyevzaojAjIis_V4yapGI6QM7jOsmkWE9B64-v4'),
+('3e60f30a-d3e7-4f26-ae70-9a367c16d609', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2ODczODk3NDksImV4cCI6MTcxODkyNTc0OSwic3ViIjoiM2U2MGYzMGEtZDNlNy00ZjI2LWFlNzAtOWEzNjdjMTZkNjA5In0.y0Uz746Ar5cAE0JmJqoxRW3ciozYUbp-Qwzay3enMK8');
 insert into `field_type` (id, type_name) values (1, 'STRING'), (2, 'INT'), (3, 'DATE');
